9ca321c7ac1badcbf1080f03aa8d1ad5250b7181
Caleb Lopresto <caleb.lopresto30@gmail.com>
2021-06-25 14:58:24 +0000

    edits for style

--- backend/api/transactions.model.js
+++ backend/api/transactions.model.js
@@ -1,16 +1,16 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 const { Schema, model } = mongoose;
 
 const schema = new Schema({
-    date: {type: Date, default: Date.now()},
-    debit: Boolean,
-    total: Number,
-    change: Number,
-    description: String,
-    store: String,
-    user: String
-})
+  date: { type: Date, default: Date.now() },
+  debit: Boolean,
+  total: Number,
+  change: Number,
+  description: String,
+  store: String,
+  user: String,
+});
 
-const Transactions = model('transaction', schema);
+const Transactions = model("transaction", schema);
 
 export default Transactions;
--- backend/api/transactions.route.js
+++ backend/api/transactions.route.js
@@ -1,36 +1,40 @@
-import express from 'express';
-import Transactions from './transactions.model.js';
+import express from "express";
+import Transactions from "./transactions.model.js";
+import Totals from "./totals.model.js";
 
 const router = express.Router();
 
-router.route('/transactions')
-    .get(
-        (req, res) => {
-            Transactions.find({}, (err, transaction) => {
-                res.send(transaction)
-            })
-        }
-    )
-    .post(
-        (req, res) => {
-            Transactions.create(req.body, (err, transaction) => {
-                res.send(transaction)
-            })
-        }
-    )
-    .put(
-        (req, res) => {
-            Transactions.findByIdAndUpdate(req.query.id, req.body, (err, transaction) => {
-                res.send(transaction)
-            })
-        }
-    )
-    .delete(
-        (req, res) => {
-            Transactions.findByIdAndRemove(req.query._id, (err, transaction) => {
-                res.send({worked: true})
-            })
-        }
-    )
+router
+  .route("/transactions")
+  .get((req, res) => {
+    Transactions.find({}, (err, transaction) => {
+      res.send(transaction);
+    });
+  })
+  .post((req, res) => {
+    Transactions.create(req.body, (err, transaction) => {
+      res.send(transaction);
+    });
+  })
+  .put((req, res) => {
+    Transactions.findByIdAndUpdate(
+      req.query.id,
+      req.body,
+      (err, transaction) => {
+        res.send(transaction);
+      }
+    );
+  })
+  .delete((req, res) => {
+    Transactions.findByIdAndRemove(req.query._id, (err, transaction) => {
+      res.send({ worked: true });
+    });
+  });
+
+router.route("/totals").get((req, res) => {
+  Totals.find({}, (err, totals) => {
+    res.send(totals);
+  });
+});
 
 export default router;
