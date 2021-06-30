f7294b9258964ab62315d30ec0adb5276d5112a1
Caleb Lopresto <caleb.lopresto30@gmail.com>
2021-06-30 20:20:38 +0000

    adds cors package

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,26 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import router from './api/transactions.route.js';
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import router from "./api/transactions.route.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
-app.use("/api/v1/", router)
-app.use("*", (req, res) => res.status(404).json({ error: "not found"}))
-
+app.use("/api/v1/", router);
+app.use("*", (req, res) => res.status(404).json({ error: "not found" }));
 
 const PORT = process.env.PORT || 8000;
 
 mongoose.connect(process.env.BUDGET_DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  })
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
 
-app.listen(PORT, () => { console.log(`Server is on ${PORT}`);});
+app.listen(PORT, () => {
+  console.log(`Server is on ${PORT}`);
+});
