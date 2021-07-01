174cab33b52968ade66ca473f55456772cbe780b
Caleb Lopresto <caleb.lopresto30@gmail.com>
2021-07-01 02:59:59 +0000

    converts to class

--- frontend/src/components/Monitor.js
+++ frontend/src/components/Monitor.js
@@ -1,33 +1,53 @@
+import { Component } from "react";
+import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Monitor = () => {
-  return (
-    <div class="container">
-      <div class="row mb-3">
-        <div class="col-12 col-sm-4 text-center">
-          <FontAwesomeIcon icon="plus" size="5x" color="green" />
-          <h3>
-            <u>Credits</u>
-          </h3>
-          <h4>$ 2000</h4>
-        </div>
-        <div class="col-12 col-sm-4 text-center">
-          <FontAwesomeIcon icon="minus" size="5x" color="red" />
-          <h3>
-            <u>Debits</u>
-          </h3>
-          <h4>$ 50</h4>
-        </div>
-        <div class="col-12 col-sm-4 text-center">
-          <FontAwesomeIcon icon="dollar-sign" size="5x" color="green" />
-          <h3>
-            <u>Balance</u>
-          </h3>
-          <h4>$ 1950</h4>
+export default class Monitor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    axios.get(`http://192.168.1.98:5000/api/v1/totals`).then((res) => {
+      console.log(res.data);
+      const data = res.data;
+      this.setState({
+        credits: data.credits,
+        debits: data.debits,
+        change: data.change,
+      });
+      console.log(this.state);
+    });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row mb-3">
+          <div className="col-12 col-sm-4 text-center">
+            <FontAwesomeIcon icon="plus" size="5x" color="green" />
+            <h3>
+              <u>Credits</u>
+            </h3>
+            <h4>$ {this.state.credits}</h4>
+          </div>
+          <div className="col-12 col-sm-4 text-center">
+            <FontAwesomeIcon icon="minus" size="5x" color="red" />
+            <h3>
+              <u>Debits</u>
+            </h3>
+            <h4>$ {this.state.debits}</h4>
+          </div>
+          <div className="col-12 col-sm-4 text-center">
+            <FontAwesomeIcon icon="dollar-sign" size="5x" color="green" />
+            <h3>
+              <u>Balance</u>
+            </h3>
+            <h4>$ {this.state.credits - this.state.debits}</h4>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
-
-export default Monitor;
+    );
+  }
+}
