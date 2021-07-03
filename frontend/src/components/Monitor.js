09ca9d22fddf80c7fc31f77bdab4437c404be76e
Caleb Lopresto <caleb.lopresto30@gmail.com>
2021-07-03 02:54:42 +0000

    Adds change to monitor

--- frontend/src/components/Monitor.js
+++ frontend/src/components/Monitor.js
@@ -15,6 +15,7 @@ export default class Monitor extends Component {
         credits: data.credits,
         debits: data.debits,
         change: data.change,
+        date: data.date,
       });
     });
   }
@@ -22,27 +23,34 @@ export default class Monitor extends Component {
   render() {
     return (
       <div className="container">
-        <div className="row mb-3">
-          <div className="col-12 col-sm-4 text-center">
+        <div className="row mb-5">
+          <div className="col-12 col-sm-3 text-center">
             <FontAwesomeIcon icon="plus" size="5x" color="green" />
             <h3>
               <u>Credits</u>
             </h3>
             <h4>$ {this.state.credits}</h4>
           </div>
-          <div className="col-12 col-sm-4 text-center">
+          <div className="col-12 col-sm-3 text-center">
             <FontAwesomeIcon icon="minus" size="5x" color="red" />
             <h3>
               <u>Debits</u>
             </h3>
             <h4>$ {this.state.debits}</h4>
           </div>
-          <div className="col-12 col-sm-4 text-center">
+          <div className="col-12 col-sm-3 text-center">
             <FontAwesomeIcon icon="dollar-sign" size="5x" color="green" />
             <h3>
               <u>Balance</u>
             </h3>
             <h4>$ {this.state.credits - this.state.debits}</h4>
+          </div>
+          <div className="col-12 col-sm-3 text-center">
+            <FontAwesomeIcon icon="coins" size="5x" color="green" />
+            <h3>
+              <u>Change</u>
+            </h3>
+            <h4>$ {this.state.change}</h4>
           </div>
         </div>
       </div>
