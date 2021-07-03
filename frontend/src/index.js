df7a63327eddead8298a11b95a95b892a4f0c9fd
Caleb Lopresto <caleb.lopresto30@gmail.com>
2021-07-03 02:54:23 +0000

    removes unnecessary imports

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,22 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import $ from "jquery";
-import Popper from "popper.js";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 import App from "./App";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faPlus,
   faMinus,
   faDollarSign,
   faEdit,
   faTrash,
+  faCoins,
 } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faPlus, faMinus, faDollarSign, faEdit, faTrash);
+library.add(faPlus, faMinus, faDollarSign, faEdit, faTrash, faCoins);
 
 ReactDOM.render(
   <React.StrictMode>
