12c2a8f150259e51c845333a763dfb9e3d6b7b2c
Caleb Lopresto <caleb.lopresto30@gmail.com>
2021-07-03 02:53:33 +0000

    adds reset component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Nav from "./components/Nav";
 import Monitor from "./components/Monitor";
 import Router from "./components/Router";
+import Reset from "./components/Reset";
 
 const App = () => {
   return (
     <div className="App">
       <Nav />
       <Monitor />
+      <Reset />
       <Router />
     </div>
   );
